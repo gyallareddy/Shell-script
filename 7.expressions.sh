076b3dcb168ee5bc43be12993d9c7875459c696e
gyallareddy <yallareddy3690@gmail.com>
2026-04-29 18:04:22 +0530

    new

--- 7.expressions.sh
+++ 7.expressions.sh
@@ -7,3 +7,12 @@ if [ $number -lt 10 ] ; then
 else
     echo number is high
 fi
+#-lt  Less than
+# -gt greater than
+# -eq equals to 
+# -ne not equal
+if [ 1 -gt 10 ]; then 
+    echo "given number is higher than the value"
+else
+    echo "given number is less"
+fi
