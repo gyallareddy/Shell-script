183f3bbdbb0af86fca2de013fed2db50e8c242e2
Yallareddy <yallareddy@Yallareddys-MacBook-Air.local>
2026-05-04 16:48:05 +0530

    update

--- 1.conditions.sh
+++ 1.conditions.sh
@@ -4,6 +4,9 @@ echo "enter any number"
 read number
 if [ $number -lt 5 ]; then
     echo "number is less than 5"
+elif [$number -gt 5 ]; then
+    echo "given number is gretaer the the actual value"
 else
     exho "number is greater than 5"
 fi
+## this is the end of the script
